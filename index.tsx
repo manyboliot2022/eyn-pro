ecec11008ee09a5dc270532f0ab17e2442a5f623
manyboliot2022 <118869747+manyboliot2022@users.noreply.github.com>
2025-12-27 01:48:18 +0000

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  console.error("Impossible de trouver l'élément root");
+}
